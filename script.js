e257ebce978a0971cff2c992c0a61de167a43100
anuragbhardwaj21 <anurag.bhardwaj_cs18@gla.ac.in>
2023-06-22 02:38:39 +0530

    master

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import navbar from "./components.js";
-const nav=document.getElementById("nav-menu");
+const nav = document.getElementById("nav-menu");
 console.log(nav);
-nav.innerHTML=navbar();
+nav.innerHTML = navbar();
 
 const homeLeft = document.getElementById("home-left");
 const homeRight = document.getElementById("home-right");
@@ -9,23 +9,27 @@ const homeRight = document.getElementById("home-right");
 homeLeft.addEventListener("mouseenter", () => {
   homeLeft.style.width = "60%";
   homeRight.style.width = "40%";
-  homeRight.style.opacity="0";
+  homeRight.style.opacity = "0";
 });
 
 homeLeft.addEventListener("mouseleave", () => {
   homeLeft.style.width = "50%";
   homeRight.style.width = "50%";
-  homeRight.style.opacity="1";
+  homeRight.style.opacity = "1";
 });
 
 homeRight.addEventListener("mouseenter", () => {
   homeLeft.style.width = "40%";
   homeRight.style.width = "60%";
-  homeLeft.style.opacity="0";
+  homeLeft.style.opacity = "0";
 });
 
 homeRight.addEventListener("mouseleave", () => {
   homeLeft.style.width = "50%";
   homeRight.style.width = "50%";
-  homeLeft.style.opacity="1";
+  homeLeft.style.opacity = "1";
+});
+window.addEventListener("load", () => {
+  homeLeft.style.animation = "slide-from-left 1s forwards";
+  homeRight.style.animation = "slide-from-right 1s forwards";
 });
