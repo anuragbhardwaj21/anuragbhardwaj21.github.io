9ecf6fc7f69293f258aac0b462cb8350792f21c3
anuragbhardwaj21 <anurag.bhardwaj_cs18@gla.ac.in>
2023-06-22 14:12:37 +0530

    master

--- components.js
+++ components.js
@@ -5,6 +5,6 @@ const navbar = () => {
   <a href="#skills" class="nav-link skills">Skills</a>
   <a href="#projects" class="nav-link projects">Projects</a>
   <a href="#contact" class="nav-link contact">Contact</a>
-  <a href="#" target="_blank" class="nav-link resume">Resume</a>`;
+  <a href="#" target="_blank" class="nav-link resume" download="Anurag_CV.pdf">Resume <ion-icon name="download-outline"></ion-icon></a>`;
 };
 export default navbar;
--- script.js
+++ script.js
@@ -33,3 +33,16 @@ window.addEventListener("load", () => {
   homeLeft.style.animation = "slide-from-left 1s forwards";
   homeRight.style.animation = "slide-from-right 1s forwards";
 });
+
+var data = [
+  {
+    name: "SkinStore",
+    link: "https://skinstoreclone7.netlify.app/",
+    desc: "One of the first online stores to offer dermatologist-created and recommended products worldwide.",
+  },
+  {
+    name: "Healthcare 24/7",
+    link: "https://skinstoreclone7.netlify.app/",
+    desc: "Convenient, reliable, and accessible online medical store delivering healthcare essentials 24/7.",
+  },
+];
