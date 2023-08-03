1ed3e3dccc6311f3d093080cb1bc830f89618d34
Anurag Bhardwaj <anuragmac@Anurags-MacBook-Air.local>
2023-08-03 21:05:21 +0530

    master

--- script.js
+++ script.js
@@ -64,42 +64,6 @@ projectVideo1.addEventListener("mouseover", a);
 projectVideo1.addEventListener("mouseout", b);
 
 // ---------------------------------->
-const ctx = document.getElementById("chart");
-new Chart(ctx, {
-  type: "doughnut",
-  data: {
-    labels: [
-      "Python",
-      "Java",
-      "JavaScript",
-      "React",
-      "HTML",
-      "CSS",
-      "Adobe Premire Pro",
-      "Adobe After Effects",
-      "Selenium",
-      "Node.js",
-    ],
-    datasets: [
-      {
-        data: [2, 20, 15, 8, 10, 10, 15, 6, 8, 6],
-        borderWidth: 4,
-        weight: 15,
-        spacing: 15,
-      },
-    ],
-  },
-  options: {
-    plugins: {
-      legend: {
-        display: false,
-      },
-    },
-    borderWidth: 12,
-    borderRadius: 5,
-    hoverBorderWidth: 0,
-  },
-});
 
 // --------------_>
 // const resume = document.getElementById("resume-button-1")
